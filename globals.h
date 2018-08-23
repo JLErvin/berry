85e4a49f1553450b585784291f4ebe30f50501fb
Joshua Ervin <jlervin@posteo.net>
2018-08-23 11:09:44 -0700

    Added globals

---
+++ globals.h
@@ -0,0 +1,11 @@
+#ifndef __HERBSTLUFT_GLOBALS_H_
+#define __HERBSTLUFT_GLOBALS_H_
+
+#define BERRY_AUTOSTART "berry/autostart"
+#define WINDOW_MANAGER_NAME "berry"
+#define __THIS_VERSION__ ""
+
+#define MAXLEN 256
+
+#endif
+
