b5523a587b62a13b3c83672b48eb536a47dbf37a
Joshua Ervin <jlervin@posteo.net>
2018-08-23 12:24:31 -0700

    Added version support from cli

--- Makefile
+++ Makefile
@@ -1,7 +1,12 @@
 include config.mk
 
+NAME_DEFINES = -D__NAME__=\"$(__NAME__)\"                 \
+			   -D__NAME_CLIENT__=\"$(__NAME_CLIENT__)\"   \
+			   -D__THIS_VERSION__=\"$(__THIS_VERSION__)\" \
+			   -D__CONFIG_NAME__=\"$(__CONFIG_NAME__)\"   \
+
 PREFIX?=/usr/X11R6
-CFLAGS?=-Os -pedantic -Wall
+CFLAGS?=-Os -pedantic -Wall $(NAME_DEFINES)
 
 all:
 	$(CC) $(CFLAGS) -I$(PREFIX)/include wm.c -L$(PREFIX)/lib -lX11 -lm -o berry
@@ -14,3 +19,4 @@ install:
 
 clean:
 	rm -f berry
+	rm -f berryc
--- config.mk
+++ config.mk
@@ -1,13 +1,14 @@
 __NAME__ = berry
 __NAME_CLIENT__ = berryc
 __CONFIG_NAME__ = autostart
-VERCMD ?= git describe 2> /dev/null
-__THIS_VERSION__ = $(shell $(VERCMD) || cat VERSION)
+__THIS_VERSION__ = $(shell cat VERSION)
 
 PREFIX    = /usr/local
 MANPREFIX = $(PREFIX)/share/man
 MANDIR    = $(MANPREFIX)/man1
 DOCPREFIX = $(PREFIX)/share/doc
+X11INC    = /usr/X11R6/include
+X11LIB    = /usr/X11R6/lib
 
 CFLAGS? += -Os -pedantic -Wall
-LDFLAGS +=  -lX11 -lm
+LDFLAGS += -lm -lX11 -I${X11INC} -L${X11LIB}
